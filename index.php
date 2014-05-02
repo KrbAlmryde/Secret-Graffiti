6257646c11bc61229859f3b6f53e6788c8b77c11
Paul Murray <plmrry@gmail.com>
2014-05-01 18:07:29 -0700

    added draw.php

--- index.php
+++ index.php
@@ -3,40 +3,11 @@
     <head>
         <title>Secret Graffiti</title>
         <script src="http://code.jquery.com/jquery-latest.js"></script>
-        <script src="js/sketch.js"></script>
-        <link href='http://fonts.googleapis.com/css?family=Shadows+Into+Light' rel='stylesheet' type='text/css'>
-
-        <style type='text/css'>
-            html, body {
-                overflow: hidden;
-            }
-
-            simple_sketch {
-                background: #04B4AE;
-            }
-            #canvas {
-                display: block;
-            }
-    </style>
 
     </head>
     <body>
-        <div class='demo'>
-          <canvas id='simple_sketch' width='auto' height='auto'></canvas>
-          <script type='text/javascript'>
-            var canvas = document.getElementById('simple_sketch');
-
-            // resize the canvas to fill browser window dynamically
-            window.addEventListener('resize', resizeCanvas, false);
-
-            function resizeCanvas() {
-                canvas.width = window.innerWidth;
-                canvas.height = window.innerHeight;
-                $('#simple_sketch').sketch();
-            }
-            resizeCanvas();
-            $('#simple_sketch').sketch();
-          </script>
+        <h1>Secret Graffiti</h1>
+        <a href="./draw.php"><button>Draw</button></a>
 
           <script>
               console.log("hello dudes.")
