8207373c2a4a8482d720e7995b234eb4737637ca
shlokadesai <shlokadesai@email.arizona.edu>
2014-05-03 14:45:58 -0700

    added options for colors, sizes, and saving

--- draw.php
+++ draw.php
@@ -17,15 +17,40 @@
             #canvas {
                 display: block;
             }
+			.tools { margin-bottom: 10px; }
+			.tools a {
+				border:1px solid #E0E0E0; height: 25px; line-height: 25px; padding: 0 10px; vertical-align: middle; text-align: center; text-decoration: none; display: inline-block; color: black;
+			}
+			
     </style>
 
     </head>
     <body>
-        <a href="./index.php"><button>Save</button></a>
+		
+		
+        <div class='tools'>
+            <button onclick="save" style="float: right; width: 100px;">Download</button>
+         </div>
+          <script>
+			function save() {
+				var canvas = document.getElementById('colors_sketch');
+				var dataURL = canvas.toDataURL();
+				$.ajax({
+					type: "POST",
+					url: "script.php",
+					data: { 
+						imgBase64: dataURL
+					}
+				}).done(function(o) {
+					console.log('saved'); 
+				});
+			}
+		</script>
+          
         <div class='demo'>
-          <canvas id='simple_sketch' width='auto' height='auto'></canvas>
+          <canvas id='colors_sketch' width='auto' height='auto'></canvas>
           <script type='text/javascript'>
-            var canvas = document.getElementById('simple_sketch');
+            var canvas = document.getElementById('colors_sketch');
 
             // resize the canvas to fill browser window dynamically
             window.addEventListener('resize', resizeCanvas, false);
@@ -33,10 +58,21 @@
             function resizeCanvas() {
                 canvas.width = window.innerWidth;
                 canvas.height = window.innerHeight;
-                $('#simple_sketch').sketch();
+                $('#colors_sketch').sketch();
             }
             resizeCanvas();
-            $('#simple_sketch').sketch();
+            $('#colors_sketch').sketch();
+          </script>
+		  <script type='text/javascript'>
+            $(function() {
+              $.each(['#424F5F', '#017B90', '#CAC6C3', '#96BD3C', '#C4D525', '#FFC90B', '#FF8730', '#FA031E', '#A43C3D', '#DA4F6E', '#593C50', '#B88B52', '#FFFFFF', '#000000'], function() {
+                $('.tools').append("<a href='#colors_sketch' data-color='" + this + "' style='width: 3px; background: " + this + ";'></a> ");
+              });
+              $.each([3, 5, 10, 15], function() {
+                $('.tools').append("<a href='#colors_sketch' data-size='" + this + "' style='background: #E0E0E0'; width: 3px>" + this + "</a> ");
+              });
+              $('#colors_sketch').sketch();
+            });
           </script>
 
         </div>
