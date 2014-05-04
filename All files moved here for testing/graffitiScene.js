7e18214a36caf29dc1c21b593645ccb496d16c60
Kyle Reese Almryde <kyle.almryde@gmail.com>
2014-05-03 20:13:59 -0700

    the world looks good

--- All files moved here for testing/graffitiScene.js
+++ All files moved here for testing/graffitiScene.js
@@ -6,32 +6,61 @@ var plane;
 var image;
 var images; // This is in anticipation for an array of images
 
+var colors = [0xFF0000, 0x00FF00, 0x0000FF, 0x00FFFF, 0xFF0000, 0x00FF00, 0x0000FF, 0x00FFFF, 0x808080];
+
 onCreate();
 onFrame();
+
+
+/*******************************************************************************/
+/***************************** FUNCTION DEFINITION *****************************/
+/*******************************************************************************/
 
 function onCreate() {
     /***************************** SETUP SCENE *****************************/
     // Create the scene
     scene = new THREE.Scene();
+    // scene.fog = new THREE.FogExp2( 0xffffff, 0.015 )
 
     /***************************** SETUP CAMERA *****************************/
 
-    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0, 1000);
-    camera.position.set(0,0,10)
-    // camera.rotation.y = -10;
-    camera.lookAt(scene.position);
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+    camera.lat = Math.random();
+    camera.lng = Math.random();
+    camera.position.set(0,2,0);
+    // camera.lookAt(scene.position);
+    // camera.lookAt(new THREE.Vector3(0,2,0));
+    camera.lookAt(camera.position);
     scene.add(camera);
 
 
     /**************************** SETUP PLANE ****************************/
-    plane = new THREE.Mesh(new THREE.PlaneGeometry( 500, 500 ),
-                           new THREE.MeshBasicMaterial(
-                                {color: 0x808080,
-                                 side: THREE.DoubleSide} )
-                           );
-    plane.rotation.x = -1.8;
-    scene.add( plane );
+    var positions = [{x:0, y:0, z:100},
+                     {x:100, y:0, z:100},
+                     {x:-100, y:0, z:100},
 
+                     {x:0, y:0, z:0},
+                     {x:100, y:0, z:0},
+                     {x:-100, y:0, z:0},
+
+                     {x:0, y:0, z:-100},
+                     {x:100, y:0, z:-100},
+                     {x:-100, y:0, z:-100}];
+
+    for (var i = 0; i < 9; i++) {
+        var aPlane = new THREE.Mesh(new THREE.PlaneGeometry( 100, 100 ),
+                               new THREE.MeshBasicMaterial(
+                                    {color: colors[i],
+                                     side: THREE.DoubleSide} )
+                               );
+        aPlane.position.set(
+                                positions[i].x,
+                                positions[i].y,
+                                positions[i].z
+                            );
+        aPlane.rotation.x = Math.PI/2; //angles[i];
+        scene.add( aPlane );
+    }
     /***************************** SETUP SPHERE *****************************/
     // create the sphere's material
     // var sphereMaterial = new THREE.MeshLambertMaterial( { color: 0xCC0000 } );
@@ -48,40 +77,20 @@ function onCreate() {
     /***************************** SETUP IMAGE(S) *****************************/
       // material
       THREE.ImageUtils.crossOrigin = "anonymous";
-      // var material = new THREE.MeshLambertMaterial({
-      //   map: THREE.ImageUtils.loadTexture('pic1.jpg'),
-      //   side: THREE.DoubleSide
-      // });
 
-      // // image
-      // image = new THREE.Mesh(new THREE.PlaneGeometry(200, 200), material);
-      // // image.overdraw = true;
-      // // image.needsUpdate = true;
-      // image.position.x = 100;
-      // image.position.y = 100;
-      // image.rotation.x = 0;
-      // scene.add(image);
       for (var i = 0; i < 10; i++) {
-          var pos = {x: i*20, y:10, z:i*10};
-          var theta = 0;
+          var pos = {x: randCoord(), y:1, z:randCoord()};
+          var theta = Math.random();
           initImage('pic1.jpg',pos, theta );
       };
-
-    // var imgTexture = new THREE.ImageUtils.loadTexture("crate.jpg");
-    // var imgGeometry = new THREE.PlaneGeometry( 216, 216 );
-    // var imgMaterial = new THREE.MeshBasicMaterial( { map: imgTexture, side: THREE.DoubleSide } );
-    // var imgMesh = new THREE.Mesh(imgGeometry, imgMaterial);
-    // imgMesh.position.x = 50;
-    // // img.map.needsUpdate = true; //ADDED
-    // scene.add( imgMesh );
 
     /***************************** SETUP PointLight *****************************/
     var pointLight = new THREE.PointLight( 0xFFFFFF );
 
     // set its position
-    pointLight.position.x = 10;
-    pointLight.position.y = 50;
-    pointLight.position.z = 130;
+    pointLight.position.x = 0;
+    pointLight.position.y = 100;
+    pointLight.position.z = 0;
     scene.add(pointLight);
 
     /***************************** SETUP RENDERER *****************************/
@@ -105,49 +114,17 @@ function onCreate() {
     // attach the render-supplied DOM element
     // container.keydown(checkKey);
     window.addEventListener('keydown', checkKey, false);
+    window.addEventListener( 'resize', onWindowResize, false );
 
     container.append(renderer.domElement);
 
     $(window).load(renderScene);
 }
 
-function initImage(fname, pos, theta){
-      var material = new THREE.MeshLambertMaterial({
-                        map: THREE.ImageUtils.loadTexture(fname),
-                        side: THREE.DoubleSide
-                    });
-
-      // image
-      var img = new THREE.Mesh(new THREE.PlaneGeometry(20, 20), material);
-      // img.overdraw = true;
-      // img.needsUpdate = true;
-      img.position.x = pos.x;
-      img.position.y = pos.y;
-      img.rotation.x = theta;
-      scene.add(img);
-
-}
-
 function onFrame() {
-
-    // plane.rotation.x += 0.01;
-    // image.rotation.x += 0.01;
-    // // plane.rotation.z += 0.02;
-    // console.log("camera.rotation.z: ", camera.rotation.z);
-    // console.log("\tcamera.rotation.x: ", camera.rotation.x);
-    // console.log("\t\tcamera.rotation.y: ", camera.rotation.y);
-    // camera.rotation.y += 0.01;
     camera.updateProjectionMatrix();
-    // if (camera.position.z > 1000 || camera.position.z < 0)
-    //     moveForward = !moveForward;
-
-    // if (maveForward) {
-    //     camera.position.z += 1;
-    // } else{
-    //     camera.position.z -= 1;
-    // };
-
     requestAnimationFrame(onFrame);
+
     // draw!
     renderScene();
 }
@@ -156,77 +133,141 @@ function renderScene() {
     renderer.render(scene, camera);
 }
 
+function randCoord() {
+  return Math.floor(Math.random() * (100 - -100 + 1)) + -100;
+}
+
+
+function initImage(fname, pos, theta){
+      var material = new THREE.MeshBasicMaterial({
+                                map: THREE.ImageUtils.loadTexture(fname),
+                                side: THREE.DoubleSide});
+
+      // image
+      var img = new THREE.Mesh(new THREE.PlaneGeometry(20, 20), material);
+      // img.overdraw = true;
+      // img.needsUpdate = true;
+      img.position.x = pos.x;
+      img.position.y = 10;
+      img.position.z = pos.z;
+      img.rotation.y = theta;
+      scene.add(img);
+
+}
+
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderScene();
+}
+
 
 function checkKey(e) {
 
     e = e || window.event;
 
-    if (e.keyCode == '104') {
-        console.log("8/camera.position.z: ",camera.position.z);
-        camera.position.z += 1;
-    }
-    else if (e.keyCode == '101') {
-        console.log("5/camera.position.z: ",camera.position.z);
-        camera.position.z -= 1;
-    }
-    else if (e.keyCode == '100') {
-        console.log("4/camera.position.x: ",camera.position.x);
-        camera.position.x -= 1;
-    }
-    else if (e.keyCode == '102') {
-        console.log("6/camera.position.x: ",camera.position.x);
-        camera.position.x += 1;
-    }
+    switch(e.keyCode){
+        case 104:
+            camera.position.z -= 1;
+            console.log("8/camera.position.z: ",camera.position.z);
+            // camera.lookAt(camera.position);
+            break;
+        case 101:
+            camera.position.z += 1;
+            console.log("5/camera.position.z: ",camera.position.z);
+            // camera.lookAt(camera.position);
+            break;
+        case 100:
+            camera.position.x -= 1;
+            console.log("4/camera.position.x: ",camera.position.x);
+            // camera.lookAt(camera.position);
+            break;
+        case 102:
+            camera.position.x += 1;
+            console.log("6/camera.position.x: ",camera.position.x);
+            // camera.lookAt(camera.position);
+            break;
 
-    else if (e.keyCode == '107') {
-        console.log("+/camera.position.y: ",camera.position.y);
-        camera.position.y += 1;
-    }
-    else if (e.keyCode == '109') {
-        console.log("-/camera.position.y: ",camera.position.y);
-        camera.position.y -= 1;
-    }
+        case 107:
+            camera.position.y -= 1;
+            console.log("+/camera.position.y: ",camera.position.y);
+            // camera.lookAt(camera.position);
+            break;
+        case 109:
+            camera.position.y += 1;
+            console.log("-/camera.position.y: ",camera.position.y);
+            // camera.lookAt(camera.position);
+            break;
 
 
 
+        case 97:
+            camera.rotation.z += 0.01;
+            console.log("0/camera.rotation.z: ",camera.rotation.z);
+            // camera.lookAt(camera.rotation);
+            break;
 
+        case 99:
+            camera.rotation.z -= 0.01;
+            console.log("2/camera.rotation.z: ",camera.rotation.z);
+            // camera.lookAt(camera.rotation);
+            break;
 
+        case 103:
+            camera.rotation.y += 0.01;
+            console.log("7/camera.rotation.y: ",camera.rotation.y);
+            // camera.lookAt(camera.rotation);
+            break;
 
-    else if (e.keyCode == '97') {
-        console.log("0/camera.rotation.z: ",camera.rotation.z);
-        camera.rotation.z -= 0.01;
-    }
-    else if (e.keyCode == '99') {
-        console.log("2/camera.rotation.z: ",camera.rotation.z);
-        camera.rotation.z += 0.01;
-    }
+        case 105:
+            camera.rotation.y -= 0.01;
+            console.log("9/camera.rotation.y: ",camera.rotation.y);
+            // camera.lookAt(camera.rotation);
+            break;
 
-    else if (e.keyCode == '103') {
-        console.log("7/camera.rotation.y: ",camera.rotation.y);
-        camera.rotation.y -= 0.01;
-    }
-    else if (e.keyCode == '105') {
-        console.log("9/camera.rotation.y: ",camera.rotation.y);
-        camera.rotation.y += 0.01;
-    }
-    else if (e.keyCode == '96') {
-        console.log("0/camera.rotation.x: ",camera.rotation.x);
-        camera.rotation.x -= 0.01;
-    }
-    else if (e.keyCode == '98') {
-        console.log("2/camera.rotation.x: ",camera.rotation.x);
-        camera.rotation.x += 0.01;
-    }
+        case 96:
+            camera.rotation.x += 0.01;
+            console.log("0/camera.rotation.x: ",camera.rotation.x);
+            // camera.lookAt(camera.rotation);
+            break;
 
-    else if (e.keyCode == '32') {
-        console.log("\ncamera.position.y: ",camera.position.y);
-        console.log("camera.position.x: ",camera.position.x);
-        console.log("camera.position.z: ",camera.position.z);
-        console.log("camera.rotation.y: ",camera.rotation.y);
-        console.log("camera.rotation.x: ",camera.rotation.x);
-        console.log("camera.rotation.z: ",camera.rotation.z);
-        console.log("\n");
+        case 98:
+            camera.rotation.x -= 0.01;
+            console.log("2/camera.rotation.x: ",camera.rotation.x);
+            // camera.lookAt(camera.rotation);
+            break;
 
+        case 82:
+            console.log("\nResetting Camera rotation and location");
+            camera.position.x = 0;
+            camera.position.y = 0;
+            camera.position.z = 0;
+
+            camera.rotation.x = 0;
+            camera.rotation.y = 0;
+            camera.rotation.z = 0;
+
+        case 32:
+            console.log("\ncamera.position.x: ",camera.position.x);
+            console.log("camera.position.y: ",camera.position.y);
+            console.log("camera.position.z: ",camera.position.z);
+            console.log("\ncamera.rotation.x: ",camera.rotation.x);
+            console.log("camera.rotation.y: ",camera.rotation.y);
+            console.log("camera.rotation.z: ",camera.rotation.z);
+            console.log("\nscene.position.x: ",scene.position.x);
+            console.log("scene.position.z: ",scene.position.z);
+            console.log("scene.position.y: ",scene.position.y);
+            console.log("\n");
+            break;
+
+        default:
+            console.log("");
+            break;
     }
+    // camera.lookAt(camera.position);
+    // camera.lookAt(camera.rotation);
+
 
 }
